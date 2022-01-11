58d4d42d85edddcdfb6569ed743d61f3757b0513
Rod-sc95 <rodrigo_sc95@hotmail.com>
2022-01-11 18:57:23 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,35 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+            </div>
+
+        </> /* Poderiamos utilizar uma div, pois a função só pode exportar um componente. Nesse caso,
+            utilizamos apenas o fragmente (<> </>) para retornar mais de um componente. Caso utilizassemos a DIV, 
+            iria aparecer mais uma div dentro da página, o que não ocorre com o fragmente, pois não altera em nada o visual da página*/
     );
 }
 
